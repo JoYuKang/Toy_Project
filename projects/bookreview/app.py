4e993491965c5bb8126833d5f8b3657f72a50dfd
Yu Knag <ykang9533@naver.com>
2021-06-23 00:19:38 +0900

    # 북리뷰 사이트

--- projects/bookreview/app.py
+++ projects/bookreview/app.py
@@ -1,28 +1,36 @@
 from flask import Flask, render_template, jsonify, request
+
 app = Flask(__name__)
 
 from pymongo import MongoClient
+
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
-## HTML을 주는 부분
+
+# HTML을 주는 부분
 @app.route('/')
 def home():
     return render_template('index.html')
 
-## API 역할을 하는 부분
+
+# API 역할을 하는 부분
 @app.route('/review', methods=['POST'])
 def write_review():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg': '이 요청은 POST!'})
+    title_receive = request.form['title_give']
+    author_receive = request.form['author_give']
+    review_receive = request.form['review_give']
+    # print(title_receive, author_receive, review_receive)
+    doc = {'title': title_receive, 'author': author_receive, 'review':review_receive}
+    db.bookreview.insert_one(doc)
+    return jsonify({'msg': '저장 완료!'})
 
 
 @app.route('/review', methods=['GET'])
 def read_reviews():
-    sample_receive = request.args.get('sample_give')
-    print(sample_receive)
-    return jsonify({'msg': '이 요청은 GET!'})
+
+    reviews = list(db.bookreview.find({}, {'_id':False}))
+    return jsonify({'all_reviews': reviews})
 
 
 if __name__ == '__main__':
